86c1924a4cef80d947f3e163de57f917ea4feddb
ats223 <83529682+ats223@users.noreply.github.com>
2023-01-28 10:00:54 +0900

    Delete exiftest.php

--- public/exiftest.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $exif = exif_read_data('./images/test01.jpg');
-?>
-
-<img src="./image/oshiro.jpeg" style="width: 200px"><br>
-この画像のexif情報は以下の通りです。<br>
-<?= nl2br(print_r($exif, true)); ?>
